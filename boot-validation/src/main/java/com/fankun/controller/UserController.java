ea01509279baff621fda372e09ad7bab04fb51d5
fankun <fankun@ccmtjf.com>
2019-01-22 17:47:15 +0800

    validation初始提交1

--- boot-validation/src/main/java/com/fankun/controller/UserController.java
+++ boot-validation/src/main/java/com/fankun/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.fankun.controller;
 
 import com.fankun.domain.User;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +15,14 @@ import javax.validation.Valid;
 public class UserController {
 
 
-    @PostMapping("/user/save")
+    @PostMapping("/user/save") //@Valid是为了走校验
     public User save(@Valid @RequestBody User user){
+        return user;
+    }
+
+    @PostMapping("/user/save2") //@Valid是为了走校验
+    public User save2(@RequestBody User user){
+        Assert.hasText("名称不能为空！",user.getName());
         return user;
     }
 }
